7142c7e1009f96ba913538d6c0a2a03af38bf71b
Galina <pgv21@yandex.ru>
2022-08-23 16:23:51 +0300

    saveTask1

---
+++ Seminar1_task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1>num2) 
+{
+Console.WriteLine($"Первое число {num1} большее, второе число {num2} меньшее");
+}
+else 
+{
+Console.WriteLine($"Первое число {num1} меньшее, второе число {num2} большее");
+}
